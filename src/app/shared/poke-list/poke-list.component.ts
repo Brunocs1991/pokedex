b8e08758716ad7ffd4c243e755aa513032a909a2
Bruno <brunocosta.1991@hotmail.com>
2022-04-20 11:53:55 -0300

    listagem

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -7,9 +7,12 @@ import { PokeApiService } from 'src/app/service/poke-api.service';
   styleUrls: ['./poke-list.component.scss'],
 })
 export class PokeListComponent implements OnInit {
+  public getAllPokemons: any;
   constructor(private pokeApiService: PokeApiService) {}
 
   ngOnInit(): void {
-    this.pokeApiService.apiListAllPokemons.subscribe((res) => console.log(res));
+    this.pokeApiService.apiListAllPokemons.subscribe(
+      (res) => (this.getAllPokemons = res.results)
+    );
   }
 }
